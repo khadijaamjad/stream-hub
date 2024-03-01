2cbd318b768228005897a99ae9cb12db820fbd9b
khadijaamjad <khadijah.amjad25@gmail.com>
2024-03-01 15:38:56 +0500

    Initial commit

---
+++ StreamHub/Data/StreamHubContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StreamHub.Models;
+
+namespace StreamHub.Data
+{
+    public class StreamHubContext : DbContext
+    {
+        public StreamHubContext (DbContextOptions<StreamHubContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StreamHub.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ StreamHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StreamHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

08d80d24a23390d8965072870dc387efba54e94c
khadijaamjad <khadijah.amjad25@gmail.com>
2024-03-01 16:40:36 +0500

    Added search option

--- StreamHub/Models/Movie.cs
+++ StreamHub/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StreamHub.Models
 {
@@ -6,9 +7,13 @@ namespace StreamHub.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
